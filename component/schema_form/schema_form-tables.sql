0f0e762a24a141b525cf3573431ae10fb23b784e
Maks <semenov@mail.ru>
2022-04-27 19:34:48 +0400

    fix связи у schema-form

--- component/schema_form/schema_form-tables.sql
+++ component/schema_form/schema_form-tables.sql
@@ -1,10 +1,10 @@
-CREATE TABLE schema_form (
-	id int NOT NULL GENERATED ALWAYS AS IDENTITY,
-	id_form int NOT NULL,
-	id_components int NOT NULL,
-	id_parent int NULL,
-	CONSTRAINT schema_form_pk PRIMARY KEY (id),
-	CONSTRAINT schema_form_fk FOREIGN KEY (id_form) REFERENCES component(id),
-	CONSTRAINT schema_form_fk_1 FOREIGN KEY (id_components) REFERENCES component(id),
-	CONSTRAINT schema_form_fk_2 FOREIGN KEY (id_parent) REFERENCES component(id)
+CREATE TABLE public.schema_form (
+	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	id_form int4 NOT NULL,
+	id_components int4 NOT NULL,
+	id_parent int4 NULL,
+	CONSTRAINT schema_form_pk PRIMARY KEY (id)
 );
+ALTER TABLE public.schema_form ADD CONSTRAINT schema_form_fk FOREIGN KEY (id_form) REFERENCES public.component_example(id);
+ALTER TABLE public.schema_form ADD CONSTRAINT schema_form_fk_1 FOREIGN KEY (id_components) REFERENCES public.component_example(id);
+ALTER TABLE public.schema_form ADD CONSTRAINT schema_form_fk_2 FOREIGN KEY (id_parent) REFERENCES public.component_example(id);
