831b66c32be8ff3da96012eaab4252ae97e0da1c
Maks <semenov@mail.ru>
2022-04-27 23:35:34 +0400

    fix components_example-get value

--- component/component_example/components_example-get.sql
+++ component/component_example/components_example-get.sql
@@ -6,6 +6,7 @@ CREATE OR REPLACE FUNCTION public.components_example_get(_id int)
     url varchar, 
     params_id int, 
     params_name varchar, 
+    value varchar,
     type_id int, 
     type_name varchar
     )
@@ -13,7 +14,8 @@ CREATE OR REPLACE FUNCTION public.components_example_get(_id int)
 AS $function$
 	BEGIN
      return query
-        select ce.id_component , ce."class" , ce."style", cr.url, p.id as params_id , p."name" as params_name, t.id as type_id , t."name" as type_name from component_example ce 
+        select ce.id_component , ce."class" , ce."style", cr.url, p.id as params_id , p."name" as params_name,  cp.value  as value, t.id as type_id , t."name" as type_name
+        from component_example ce 
         left join components_params cp ON cp.id_components  = ce.id
         left join component_rule cr on cr.id = cp.id_params 
         left join params p  ON p.id  = cr.id_params 
