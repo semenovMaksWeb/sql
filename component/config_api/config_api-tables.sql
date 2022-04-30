b63e8e221c5da59a493fdff71baabe43a0dede86
Maks <semenov@mail.ru>
2022-04-30 17:00:30 +0400

    config_api add PK key

--- component/config_api/config_api-tables.sql
+++ component/config_api/config_api-tables.sql
@@ -3,6 +3,7 @@ CREATE TABLE public.config_api (
 	url varchar NULL,
 	"type" varchar NULL,
 	id_component int4 NULL,
+	CONSTRAINT config_api_pk PRIMARY KEY (id),
 	CONSTRAINT element_fd_result_check CHECK ((((type)::text = 'delete'::text) OR ((type)::text = 'post'::text) OR ((type)::text = 'update'::text) OR ((type)::text = 'get'::text))),
 	CONSTRAINT config_api_fk FOREIGN KEY (id_component) REFERENCES public.component_example(id)
 );
