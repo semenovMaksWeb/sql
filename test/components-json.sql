cfec014a982a459dd39a17a2f43e84cebf5611d8
Maks <semenov@mail.ru>
2022-05-03 17:36:36 +0400

    components-get-json

--- test/components-json.sql
+++ test/components-json.sql
@@ -14,7 +14,8 @@ left join (
 			'class', ce."class", 
 			'style', ce."style",
 			'type', c."name",
-			'params', cp.params 
+			'params', cp.params,
+			'api', config_api
 			)
 	) components
 	from screen_components sc
@@ -38,10 +39,35 @@ left join (
 			left join params p  on p.id  = cr.id_params
 			left join typevar t on t.id  = p."type" 
 			group by cp.id_components 
-			)cp on cp.id_components = ce.id 	
-	
-	
-	
+			)cp on cp.id_components = ce.id 		
+	left join (
+		select ca.id_component, json_agg(
+			json_build_object(
+				'url', ca.url,
+				'type', ca.type,
+				'params', api_params
+			)
+		) config_api  from config_api ca 
+		left join (
+			select cap.id_config_api,
+			  json_agg(
+					json_build_object(
+						'name_params', ef.name_params,
+						'result', ef.result,
+						'name', ef.name,
+						'index', ef."index",
+						'type', ef."type",
+						'type_var', t2."name" 
+					)
+			) api_params 
+			from component_api_params cap 
+			left join element_fd ef ON cap.id_element_fd = ef.id
+			left join typevar t2 on t2.id = ef.var_type 
+			group by cap.id_config_api
+		) cap on cap.id_config_api = ca.id
+				group by ca.id_component
+	 ) ca  on ca.id_component = ce.id
+			
 	where sc.id_screen = 1
 		group by sc.id_screen 
 ) sc on s.id = sc.id_screen
