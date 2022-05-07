f64b8440a340a5ca58776157f5b46d492a375285
Maks <semenov@mail.ru>
2022-05-07 18:36:40 +0400

    component/component_callback add event

--- component/component_callback/component_callback-tables.sql
+++ component/component_callback/component_callback-tables.sql
@@ -1,9 +1,11 @@
 CREATE TABLE component_callback (
-	id int NOT NULL GENERATED ALWAYS AS IDENTITY,
-	id_callback int NOT NULL,
+	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	id_callback int4 NOT NULL,
 	params json NULL,
-	id_component int NULL,
+	id_component int4 NULL,
+	id_event int4 NULL,
 	CONSTRAINT component_callback_pk PRIMARY KEY (id),
+	CONSTRAINT component_callback_event_fk FOREIGN KEY (id_event) REFERENCES components."event"(id),
 	CONSTRAINT component_callback_fk FOREIGN KEY (id_callback) REFERENCES components.function_front(id),
 	CONSTRAINT component_callback_fk_1 FOREIGN KEY (id_component) REFERENCES components.component_example(id)
 );
