e056a6230f2c57abd0efdf48d9cb7718a566ebb7
Maks <semenov@mail.ru>
2022-05-07 19:00:28 +0400

    тестовая функция возвращение скрина для платформы

--- test/components-json.sql
+++ test/components-json.sql
@@ -1,13 +1,17 @@
+CREATE OR REPLACE FUNCTION screen_platform_get(_id int)
+ RETURNS  TABLE(screen json)
+ LANGUAGE plpgsql
+AS $function$
+	BEGIN
 --	главный select
-select json_build_object(
+  return query select json_build_object(
 	'screen_id', s.id,
 	'component', components
-)
-from screen s 
+) screen
+from components.screen s 
 left join (
 --	компоненты select
-	select 
-	sc.id_screen,
+	select sc.id_screen,
 	json_agg(
 			json_build_object(
 			'id', sc.id,
@@ -15,29 +19,40 @@ left join (
 			'style', ce."style",
 			'type', c."name",
 			'params', cp.params,
-			'api', config_api
-			)
-	) components
-	from screen_components sc
-	left join component_example ce on ce.id = sc.id
-	left join component c on c.id = ce.id_component
+			'api', config_api,
+			'event', component_callback
+			)) components
+	from components.screen_components sc
+	left join components.component_example ce on ce.id = sc.id
+	left join components.component c on c.id = ce.id_component
+	-- калбек + event
+left join (
+	select cc.id_component,
+	json_agg(
+		json_build_object(
+			e.name, (json_build_object(
+				'id', cc.id,
+				'params', cc.params	
+			)))) component_callback
+	from components.component_callback cc
+	left join  components.event e on e.id = cc.id_event
+	group by cc.id_component
+) cc on cc.id_component = ce.id 
 --	параметры
 		left join (
-			select
-		cp.id_components,
+			select cp.id_components,
 			json_agg(
 					json_build_object(
-						'id', p.id,
+					p."name", json_build_object(
+--						'id', p.id,
 						'url', cr.url,
-						'name', p."name",
 						'value', cp.value,
-						'type', t."name" 
-					)	
-				) params
-			from components_params cp  
-			left join component_rule cr  on cr.id  = cp.id_params
-			left join params p  on p.id  = cr.id_params
-			left join typevar t on t.id  = p."type" 
+						'type', t."name" 	
+					))) params
+			from components.components_params cp  
+			left join components.component_rule cr  on cr.id  = cp.id_params
+			left join components.params p  on p.id  = cr.id_params
+			left join components.typevar t on t.id  = p."type" 
 			group by cp.id_components 
 			)cp on cp.id_components = ce.id 		
 	left join (
@@ -47,7 +62,7 @@ left join (
 				'type', ca.type,
 				'params', api_params
 			)
-		) config_api  from config_api ca 
+		) config_api  from components.config_api ca 
 		left join (
 			select cap.id_config_api,
 			  json_agg(
@@ -58,18 +73,18 @@ left join (
 						'index', ef."index",
 						'type', ef."type",
 						'type_var', t2."name" 
-					)
-			) api_params 
-			from component_api_params cap 
-			left join element_fd ef ON cap.id_element_fd = ef.id
-			left join typevar t2 on t2.id = ef.var_type 
+					)) api_params 
+			from components.component_api_params cap 
+			left join components.element_fd ef ON cap.id_element_fd = ef.id
+			left join components.typevar t2 on t2.id = ef.var_type 
 			group by cap.id_config_api
 		) cap on cap.id_config_api = ca.id
 				group by ca.id_component
 	 ) ca  on ca.id_component = ce.id
-			
-	where sc.id_screen = 1
+	where sc.id_screen = _id
 		group by sc.id_screen 
 ) sc on s.id = sc.id_screen
-
-where s.id = 1
+where s.id = _id;
+	END;    
+$function$
+;
