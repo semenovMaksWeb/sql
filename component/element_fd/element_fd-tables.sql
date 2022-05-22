3e6d68cef6bfc89f96c0db88caebeafa065ac9cb
Semenov Maks <Semenov@mail.ru>
2022-05-22 12:26:58 +0400

    element_fd-tables add pole id_ce

--- component/element_fd/element_fd-tables.sql
+++ component/element_fd/element_fd-tables.sql
@@ -6,8 +6,9 @@ CREATE TABLE components.element_fd (
 	"name" varchar NOT NULL,
 	"index" int4 NULL,
 	var_type int4 NULL,
+	id_ce int4 NULL,
 	CONSTRAINT element_fd_pk PRIMARY KEY (id),
+	CONSTRAINT element_fd_result_check CHECK ((((result)::text = 'params'::text) OR ((result)::text = 'body'::text) OR ((result)::text = 'all'::text))),
 	CONSTRAINT element_fd_type_check CHECK ((type = ANY (ARRAY[1, 2, 3, 4]))),
-	CONSTRAINT element_fd_result_check CHECK ("result" = 'params' or  "result" = 'body' or "result" = 'all' ),
 	CONSTRAINT element_fd_fk FOREIGN KEY (var_type) REFERENCES handbook.typevar(id)
 );
