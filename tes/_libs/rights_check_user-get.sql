1bb58b38b240c8515dd358bc0a56701b5f2ed3e2
Semenov Maks <Semenov@mail.ru>
2022-05-22 18:05:38 +0400

    обработка при проверки права roles id =1 суперпользователь

--- tes/_libs/rights_check_user-get.sql
+++ tes/_libs/rights_check_user-get.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION tes.rights_check_user_get(_token uuid=NULL, _right integer=NULL)
+CREATE OR REPLACE FUNCTION tes.rights_check_user_get(_token uuid DEFAULT NULL::uuid, _right integer DEFAULT NULL::integer)
  RETURNS TABLE(check_rights bigint)
  LANGUAGE plpgsql
 AS $function$
@@ -6,7 +6,7 @@ AS $function$
         IF _right IS NULL THEN
 			return query select count(1) as check_rights;
        	ELSE
-	   		IF  _token IS NULL THEN
+	   		IF _token IS NULL THEN
 			   return query select count(NULL) as check_rights;
 			ELSE
 				return query  select count(rr.id_rights) as check_rights from "token" t 
@@ -14,7 +14,7 @@ AS $function$
 				left join tes."user" u on u.id  = tu.id_user
 				left join tes.user_roles ur on ur.id_user = u.id 
 				left join tes.roles_rights rr on rr.id_roles = rr.id_roles 
-				where t.value = _token and t.active = true and t.date > current_timestamp and u.active  = true and rr.id_rights =  _right;
+				where t.value = _token and t.active = true and t.date > current_timestamp and u.active  = true and (rr.id_rights =  _right or rr.id_roles = 1);
         	END IF;
 		END IF;
 	END;
