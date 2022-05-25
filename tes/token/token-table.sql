6804bd3f4eb59bff0c429f3ec71315e98cc25f1c
Semenov Maks <Semenov@mail.ru>
2022-05-25 16:32:45 +0400

    add

--- tes/token/token-table.sql
+++ tes/token/token-table.sql
@@ -2,7 +2,9 @@ CREATE TABLE tes."token" (
 	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	value uuid NOT NULL DEFAULT uuid_generate_v4(),
 	active bool NULL DEFAULT true,
-	"date" timestamp NOT NULL DEFAULT (CURRENT_DATE + 2),
+	"date" timestamp NOT NULL DEFAULT (now() + '2 days'::interval day),
+	id_user int4 NULL,
 	CONSTRAINT token_pk PRIMARY KEY (id),
-	CONSTRAINT token_value_key UNIQUE (value)
+	CONSTRAINT token_value_key UNIQUE (value),
+	CONSTRAINT token_fk FOREIGN KEY (id_user) REFERENCES tes."user"(id)
 );
--- tes/token_user/token_user-create.sql
+++ tes/token_user/token_user-create.sql
@@ -10,13 +10,11 @@ DECLARE
 	_id_user int = (SELECT id FROM tes."user" u WHERE u.login = _login and u.password = crypt(_password, u.password));
 	begin
         IF _id_user IS NOT NULL THEN
-            INSERT INTO tes.token DEFAULT VALUES RETURNING * INTO _token;
-            INSERT INTO tes.token_user  
-            ("id_token", "id_user")
-            VALUES(_token.id, _id_user);
+            INSERT INTO tes.token ("id_user") VALUES (id_user) RETURNING * INTO _token;
             return _token.value;
         END IF;
         return json_build_object('errors', json_build_object('password', 'Не вверно введены данные'));
 	END;
 $function$
 ;
+-- select  * from tes.token_user_create('test2','1') as result;
